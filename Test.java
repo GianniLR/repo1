3152dddafd35a0775730dc3548a23cdf0a4ecf74
Your Name <you@example.com>
2020-12-09 14:48:48 +0100

    Terza commit: committo soltanto due file Test.java e .class

--- Test.java
+++ Test.java
@@ -5,6 +5,8 @@ public static void main (String[] args){
 	System.out.println("Mia modifica");
 
 	System.out.println("Stampa aggiunta dopo");
+	
+	int a=9;
 
 	System.out.println("ULTIMA MODIFICA");
 
